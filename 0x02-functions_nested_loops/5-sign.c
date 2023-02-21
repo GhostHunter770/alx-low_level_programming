b73b41467d34ba87a444261a6e4bd89ddd37d933
GH770 <mohmed_abdallah770@hotmail.com>
2023-02-21 13:40:37 -0800

    all tasks from 3 - 11

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *
+ *
+ * return: 1 for positive num, -1 for negative num
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
