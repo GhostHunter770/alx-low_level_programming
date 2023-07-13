fcc8d3b5bfb76a4292eec0d315017e623afeb99b
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-13 16:36:42 -0700

    c tasks" q

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * return: always 0 (success)
+ * return: always (0)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
