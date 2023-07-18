4a8cbfb38b5268de4fe6e14da37c49c77cf9a8ba
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-18 15:34:01 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <main.h>
 
 /**
  * main - prints the string _putchar
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
