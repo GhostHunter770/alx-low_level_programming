c672c2ffb9380755d1a447c2307b3fa5b431078a
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-18 16:30:47 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @n: is the int to be checked.
+ *
+ * Return: 1 if alpha, otherwaise 0.
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+
+	else if (n < 0)
+		_putchar('-');
+		return (-1);
+
+	else
+		_putchar('0');
+		return (0);
+}
