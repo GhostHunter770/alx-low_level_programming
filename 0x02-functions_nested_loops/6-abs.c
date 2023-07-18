dfc607331a1d80b918df05e43e50fd7f71da3c51
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-18 16:42:42 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/** 
+ * _abs = computes the absolute calue of an integer
+ *
+ *@n: the integer to check
+ *
+ * Return:absolute value
+ */
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		return(n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
