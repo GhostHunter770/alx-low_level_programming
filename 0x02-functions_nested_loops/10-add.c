3c2bb4a80190351380db59aaf64817fda6fde0bf
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-18 17:44:53 -0700

    taks 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ *
+ * @n1: first integer.
+ *
+ * n2: second integer
+ *
+ * Return:n1+n2
+ */
+int add(int n1, int n2)
+{
+	int s = n1 + n2;
+
+	return (s);
+}
