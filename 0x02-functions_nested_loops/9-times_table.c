4187b6c6e6a39c4b524774917119ea33c9669859
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-18 17:19:09 -0700

    task 6

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,12 +6,30 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a < 10;a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10;b++)
+		for (b = 0; b < 10; b++)
 		{
 			c = a * b;
-			_putchar(c);
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			if (c < 10 && b != 0)
+			{
+				_putchar(',';
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (k >= 10)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
