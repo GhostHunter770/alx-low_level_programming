df4c33cc2a8c202c9430d8dab08e039738eff326
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-18 18:07:00 -0700

    tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,13 +20,13 @@ void print_to_98(int n)
 		}
 	}
 	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
 		{
-			for (b = n; b >= 98; b--)
-			{
-				 if (b != 98)
+			if (b != 98)
 				printf("%d, ", b);
-				else if (b == 98)
+			else if (b == 98)
 				printf("%d\n", b);
-			}
 		}
+	}
 }
