bcf49cdeb15cad6a92ce399cb727c2fe4564612f
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-20 16:45:59 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: number of lines to be printed.
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a = 0;
+	while (a <= n)
+	{
+		if(n > 0)
+		{
+		_putchar('_');
+		a++;
+		}
+		else 
+			break;
+	}
+	_putchar('\n');
+}
+
