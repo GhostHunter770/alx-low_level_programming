c327e743a2030aaf45f7bbd7596b781c611ae3da
GH770 <mohmed_abdallah770@hotmail.com>
2023-07-21 15:32:33 -0700

    task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * main - Test function for positive or negative
+*
 * Return: 0
 */
 int main(void)
@@ -8,6 +9,8 @@ int main(void)
 int i;
 
 i = 0;
+
 positive_or_negative(i);
+
 return (0);
 }
