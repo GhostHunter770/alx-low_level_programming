202e976ddd58e55b9017e6ded651c7c6ae310f9b
GH770 <mohmed_abdallah770@hotmail.com>
2023-08-24 17:26:14 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - calculates the number of  elements of a list_t list
  * @h: pointer to the head of the list
  * Return: the number of nodes
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 	const list_t *current = h;
