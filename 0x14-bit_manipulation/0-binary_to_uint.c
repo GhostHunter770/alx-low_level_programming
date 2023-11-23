a9c2e910c9011e178f65ac979e26f85039f019e6
GH770 <mohmed_abdallah770@hotmail.com>
2023-11-23 04:13:29 -0800

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	int a;
+	unsigned int num;
+
+	num = 0;
 	if (!b)
-		return 0;
-	for (; *b; b++)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
 	{
-		if (*b == '1')
-			num = (num << 1) | 1;
-		else if (*b == '0')
-			num <<= 1;
-		else
-			return 0;
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
 	}
-	return num;
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
 }
