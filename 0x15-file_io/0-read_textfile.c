150a5960b139919cfd6e9cb4b31e4ae3c5873295
GH770 <mohmed_abdallah770@hotmail.com>
2023-11-27 03:18:59 -0800

    task 0-1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fc;
+	ssize_t readout;
+	ssize_t	writeout;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fc = open(filename, O_RDONLY);
+	if (fc == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fc);
+		return (0);
+	}
+	readc = read(fd, buffer, letters);
+	close(fc);
+	if (readc == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	writec = write(STDOUT_FILENO, buffer, readc);
+	free(buffer);
+	if (readc != writec || writec == -1)
+	{
+		return (0);
+	}
+	return (writec);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
+ *
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int count = 0;
+	int fc = 0;
+	int out = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fc == -1)
+		return (-1);
+
+	if (text_ != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
+	{
+		close(fc);
+		return (1);
+	}
+
+	out = write(fc, text_content, count);
+	if (out == -1 || out != count)
+		return (-1);
+
+	close(fc);
+	return (1);
+}
